d2f619b34cf331922ec287cad16fe4934b658bb0
M <28916828+M1k3G0@users.noreply.github.com>
2021-03-21 05:48:44 +0000

    Update firewall.bat

--- firewall.bat
+++ firewall.bat
@@ -1,4 +1,5 @@
 %windir%\System32\netsh.exe advfirewall firewall delete rule all
+timeout /t 5 /nobreak > NUL
 %windir%\System32\reg.exe delete "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\SharedAccess\Parameters\FirewallPolicy\FirewallRules" /f
 %windir%\System32\reg.exe add "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\SharedAccess\Parameters\FirewallPolicy\FirewallRules" /f
 %windir%\System32\reg.exe delete "HKEY_LOCAL_MACHINE\System\ControlSet001\Services\SharedAccess\Parameters\FirewallPolicy\FirewallRules" /f
@@ -6,6 +7,7 @@
 %windir%\System32\reg.exe delete "HKEY_LOCAL_MACHINE\System\ControlSet002\Services\SharedAccess\Parameters\FirewallPolicy\FirewallRules" /f
 %windir%\System32\reg.exe add "HKEY_LOCAL_MACHINE\System\ControlSet002\Services\SharedAccess\Parameters\FirewallPolicy\FirewallRules" /f
 %windir%\System32\netsh.exe advfirewall set allprofiles firewallpolicy blockinbound,blockoutbound
+timeout /t 5 /nobreak > NUL
 %windir%\System32\reg.exe add "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\SharedAccess\Parameters\FirewallPolicy\StandardProfile" /v "DoNotAllowExceptions" /t REG_DWORD /d "1" /f
 %windir%\System32\reg.exe add "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\SharedAccess\Parameters\FirewallPolicy\DomainProfile" /v "DoNotAllowExceptions" /t REG_DWORD /d "1" /f
 %windir%\System32\reg.exe add "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\SharedAccess\Parameters\FirewallPolicy\PublicProfile" /v "DoNotAllowExceptions" /t REG_DWORD /d "1" /f
